efeca5b112366021622fbd844e4b33970d3cb627
Spion-Spion <105750231+Spion-Spion@users.noreply.github.com>
2022-08-16 15:50:10 +0200

    1.2v

--- arduino_radar.ino
+++ arduino_radar.ino
@@ -1,119 +1,98 @@
 #include <Servo.h>
 
-//zmienne serwa
 Servo serwo;
-bool strona = true; //true w prawo false w lewo
-short int kat = 0;
-
-//zmiene czasowe
 unsigned long mili_S;
 unsigned long mili_K;
 
-//piny sensora
+unsigned char znak[6]{'0','0','0','0','0','0'};
+
+// defines pins numbers
 const short int trigPin = 9;
 const short int echoPin = 10;
 
-//zmienne dotyczace pomiarow
-long czas_trwania;
-short int dystans;
+// defines variables
+long duration;
+short int distance;
 
-//zmiene dotyczace transmisji
-int dlugosc_stringu[2]{};
-char znak[6]{'0','0','0','0','0','0'};
-String napis[2];
-int roznica = 0;
+
+int kat = 0;
+
+bool strona = true; //true w prawo false w lewo
+
+String napis;
+
 
 void setup() 
 {
-  serwo.attach(2);
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  Serial.begin(9600); // Starts the serial communication
+    serwo.attach(2);
+    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+    pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+    Serial.begin(9600); // Starts the serial communication
 }
 
 
 void loop() 
 {
-  mili_S = millis();
+    mili_S = millis();
 
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(15);
+    
+    // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
      
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(15);
-  digitalWrite(trigPin, LOW);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
     
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  czas_trwania = pulseIn(echoPin, HIGH);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
     
-  // Calculating the distance
-  dystans = czas_trwania*0.034/2;
+    // Calculating the distance
+    distance= duration*0.034/2;
+    
+    // Prints the distance on the Serial Monitor
+  
+    napis = static_cast<String>(distance);
+    znak[0] = napis[0];
+    znak[1] = napis[1];
+    znak[2] = napis[2];
+    
+     if(strona == true)
+     {
+       kat++;
+     }
 
-  mili_K = millis();
-  mili_K = mili_K - mili_S;
+     if(strona == false)
+     {
+       kat--;
+     }
+     
+     if(kat >= 185)
+     {
+        strona = false;
+     }
 
-  while(mili_K <= 30)
-  {
+     if(kat <= 0)
+     {
+        strona = true;
+     }
+    napis = static_cast<String>(kat);
+    
+    znak[3] = napis[0];
+    znak[4] = napis[1];
+    znak[5] = napis[2];
+
     mili_K = millis();
     mili_K = mili_K - mili_S;
-  }
-  
-
-  //sterowanie serwem 
-  if(kat < 0)
-  {
-    strona = true;
-    kat = 0;
-  }
-  else if( kat > 180)
-  {
-    strona = false;
-    kat = 180;
-  }
-
-  if(strona)
-  {
-    kat++;
-  }
-  else
-  {
-    kat--;
-  }
-
-  //zerowanie ramki
-  for(short i=0;i<6;i++)
-  {
-    znak[i] = '0';
-  }
-
-  //przygotowanie ramki do wysłania do kompa
-  napis[1] = String(dystans);
-  dlugosc_stringu[1] = napis[1].length();
-
-  napis[0] = String(kat); 
-  dlugosc_stringu[0] = napis[0].length();
-
-  roznica = 1;
-  for(int i=2;i >= 3-dlugosc_stringu[0];i--)
-  {
-    znak[i] = napis[0][dlugosc_stringu[0]-roznica];
-
-    roznica++;
-  }
-
-  roznica = 1;
-  for(int i=5;i >= 6-dlugosc_stringu[1]; i--)
-  {
-    znak[i] = napis[1][dlugosc_stringu[1]-roznica];
-
-    roznica++;
-  }
-
-  //wysłanie ramki gdy dostanie takie info że trzeba
-  if(Serial.available())
-  {
+    
     Serial.write(znak,6);
-  }
+    
+    while(mili_K <= 30)
+    {
+      mili_K = millis();
+      mili_K = mili_K - mili_S;
+    }
+  
+    serwo.write(kat);
 }
